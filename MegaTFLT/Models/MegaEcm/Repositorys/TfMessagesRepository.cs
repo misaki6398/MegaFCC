ce3985971a0894a6ff6cd3c2adddbab32f1904b7
008958 <frankykuo@megabank.com.tw>
2021-08-20 15:38:52 +0800

    fix for using OracleDynamicParameters

--- MegaTFLT/Models/MegaEcm/Repositorys/TfMessagesRepository.cs
+++ MegaTFLT/Models/MegaEcm/Repositorys/TfMessagesRepository.cs
@@ -58,12 +58,44 @@ namespace MegaTFLT.Models.MegaEcm.Repositorys
         }
         public override async Task<int> InsertAsync(TfMessageModel model)
         {
-           model.RawMessageClob = OracleDBUtility.ConvertToClob(model.RawMessage, (Oracle.ManagedDataAccess.Client.OracleConnection)this.Connection);
-           var parameter = new OracleDynamicParameters();    
-           parameter.Add("RawMessageClob", model.RawMessageClob);
+            model.RawMessageClob = OracleDBUtility.ConvertToClob(model.RawMessage, (Oracle.ManagedDataAccess.Client.OracleConnection)this.Connection);
+            var parameter = new OracleDynamicParameters();
 
+            //byte[] newvalue = System.Text.Encoding.Unicode.GetBytes(model.RawMessage);
+            //var clob = new OracleClob((Oracle.ManagedDataAccess.Client.OracleConnection)this.Connection);
+            //clob.Write(newvalue, 0, newvalue.Length);
 
-            return await base.InsertAsync(model, _insertSql);
+            parameter.Add("id", model.id);
+            parameter.Add("RawMessageClob", model.RawMessageClob);
+            parameter.Add("MessageDefinitionIdentifier", model.MessageDefinitionIdentifier);
+            parameter.Add("BusinessMessageIdentifier", model.BusinessMessageIdentifier);
+            parameter.Add("BusinessService", model.BusinessService);
+            parameter.Add("OriginalCreateDate", model.OriginalCreateDate);
+            parameter.Add("FromId", model.FromId);
+            parameter.Add("ToId", model.ToId);
+            parameter.Add("Currency", model.Currency);
+            parameter.Add("Amount", model.Amount);
+            parameter.Add("SwallowId", model.SwallowId);
+            parameter.Add("BranchNO", model.BranchNO);
+            parameter.Add("ValidFlag", model.ValidFlag);
+            parameter.Add("CreateUser", model.CreateUser);
+            parameter.Add("CreateDatetime", model.CreateDatetime);
+            parameter.Add("UpdateUser", model.UpdateUser);
+            parameter.Add("UpdateDatetime", model.UpdateDatetime);
+
+            try
+            {
+                return await Connection.ExecuteAsync(this.InsertSql, parameter, Transaction);
+            }
+            catch (OracleException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
         }
     }
 }
